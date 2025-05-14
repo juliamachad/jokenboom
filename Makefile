65fa6e128a5d851cdd36b62a84dc9774550e0b37
Julia Machado <juliamachadosouza@gmail.com>
2025-05-14 10:47:54 -0300

    inicial

---
+++ Makefile
@@ -0,0 +1,41 @@
+# Makefile para Jokenboom (server/client)
+
+CC := gcc
+CFLAGS := -std=c11 -Wall -Wextra
+BUILDDIR := bin
+
+# Targets finais
+TARGETS := $(BUILDDIR)/server $(BUILDDIR)/client
+
+# Fontes e objetos
+SRCS_COMMON := common.c
+SRCS_SERVER := server.c
+SRCS_CLIENT := client.c
+OBJS_COMMON := common.o
+OBJS_SERVER := server.o
+OBJS_CLIENT := client.o
+
+.PHONY: all clean
+
+all: $(TARGETS)
+
+# Garante que o diretório bin/ existe antes de linkar
+$(BUILDDIR):
+	mkdir -p $@
+
+# Link do server
+$(BUILDDIR)/server: $(BUILDDIR) $(OBJS_COMMON) $(OBJS_SERVER)
+	$(CC) $(CFLAGS) -o $@ $(OBJS_SERVER) $(OBJS_COMMON)
+
+# Link do client
+$(BUILDDIR)/client: $(BUILDDIR) $(OBJS_COMMON) $(OBJS_CLIENT)
+	$(CC) $(CFLAGS) -o $@ $(OBJS_CLIENT) $(OBJS_COMMON)
+
+# Compilação de .c para .o
+%.o: %.c common.h
+	$(CC) $(CFLAGS) -c $< -o $@
+
+# Remove executáveis e objetos
+clean:
+	rm -f *.o
+	rm -rf $(BUILDDIR)
---
+++ client.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "common.h"
+
+#define MSG_SIZE 256
+#define BUFSZ 1024
+
+typedef enum {
+    MSG_REQUEST,
+    MSG_RESPONSE,
+    MSG_RESULT,
+    MSG_PLAY_AGAIN_REQUEST,
+    MSG_PLAY_AGAIN_RESPONSE,
+    MSG_ERROR,
+    MSG_END
+} MessageType;
+
+typedef struct {
+    int type;
+    int client_action;
+    int server_action;
+    int result;
+    int client_wins;
+    int server_wins;
+    char message[MSG_SIZE];
+} GameMessage;
+
+
+void usage(int argc, char **argv) {
+	printf("Uso: %s <IP> <porta>\n", argv[0]);
+	printf("exemplo: %s 127.0.0.1 51511\n", argv[0]);
+	exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        usage(argc, argv);
+    }
+
+   struct sockaddr_storage storage;
+	if (0 != addrparse(argv[1], argv[2], &storage)) {
+		usage(argc, argv);
+	}
+
+	int s;
+	s = socket(storage.ss_family, SOCK_STREAM, 0);
+	if (s == -1) {
+		logexit("socket");
+	}
+	struct sockaddr *addr = (struct sockaddr *)(&storage);
+	if (0 != connect(s, addr, sizeof(storage))) {
+		logexit("connect");
+	}
+
+	char addrstr[BUFSZ];
+	addrtostr(addr, addrstr, BUFSZ);
+
+	printf("connected to %s\n", addrstr);
+
+	char buf[BUFSZ];
+	memset(buf, 0, BUFSZ);
+	printf("mensagem> ");
+	fgets(buf, BUFSZ-1, stdin);
+	size_t count = send(s, buf, strlen(buf)+1, 0);
+	if (count != strlen(buf)+1) {
+		logexit("send");
+	}
+
+	memset(buf, 0, BUFSZ);
+	unsigned total = 0;
+	while(1) {
+		count = recv(s, buf + total, BUFSZ - total, 0);
+		if (count == 0) {
+			// Connection terminated.
+			break;
+		}
+		total += count;
+	}
+	close(s);
+
+	printf("received %u bytes\n", total);
+	puts(buf);
+
+	exit(EXIT_SUCCESS);
+}
