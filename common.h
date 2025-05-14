d017a81751a52aad494083019395bfea5606e043
Julia Machado <juliamachadosouza@gmail.com>
2025-05-14 10:58:57 -0300

    definicao de struct no header

--- common.h
+++ common.h
@@ -4,6 +4,28 @@
 
 #include <arpa/inet.h>
 
+#define MSG_SIZE 256
+
+typedef enum {
+    MSG_REQUEST,
+    MSG_RESPONSE,
+    MSG_RESULT,
+    MSG_PLAY_AGAIN_REQUEST,
+    MSG_PLAY_AGAIN_RESPONSE,
+    MSG_ERROR,
+    MSG_END
+} MessageType;
+
+typedef struct {
+    int type;
+    int client_action;
+    int server_action;
+    int result;
+    int client_wins;
+    int server_wins;
+    char message[MSG_SIZE];
+} GameMessage;
+
 void logexit(const char *msg);
 
 int addrparse(const char *addrstr, const char *portstr,
--- server.c
+++ server.c
@@ -7,29 +7,7 @@
 #include <arpa/inet.h>
 #include "common.h"
 
-#define MSG_SIZE 256
 #define BUFSZ 1024
-
-typedef enum {
-    MSG_REQUEST,
-    MSG_RESPONSE,
-    MSG_RESULT,
-    MSG_PLAY_AGAIN_REQUEST,
-    MSG_PLAY_AGAIN_RESPONSE,
-    MSG_ERROR,
-    MSG_END
-} MessageType;
-
-typedef struct {
-    int type;
-    int client_action;
-    int server_action;
-    int result;
-    int client_wins;
-    int server_wins;
-    char message[MSG_SIZE];
-} GameMessage;
-
 
 void usage(int argc, char **argv) {
     printf("Uso: %s <v4|v6> <porta>\n", argv[0]);
